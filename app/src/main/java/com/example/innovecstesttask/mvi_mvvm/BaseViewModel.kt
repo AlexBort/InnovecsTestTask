6aeea284490dab426d9294e747390fa76a33703f
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2022-11-02 11:20:09 +0200

    efforts for Hilt implementation - draft

--- app/src/main/java/com/example/innovecstesttask/mvi_mvvm/BaseViewModel.kt
+++ app/src/main/java/com/example/innovecstesttask/mvi_mvvm/BaseViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.innovecstesttask.mvi_mvvm
 
 import androidx.lifecycle.ViewModel
+import com.example.innovecstesttask.data.result_handling.DataManager
 
 abstract class BaseViewModel : ViewModel() {
     abstract fun obtainIntent(intent: BaseUserIntent)
+//    abstract fun getDataManager(): DataManager
 }
--- app/src/main/java/com/example/innovecstesttask/mvi_mvvm/MainViewModel.kt
+++ app/src/main/java/com/example/innovecstesttask/mvi_mvvm/MainViewModel.kt
@@ -3,19 +3,18 @@ package com.example.innovecstesttask.mvi_mvvm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.innovecstesttask.data.result_handling.ConfigurationDataManager
-import com.example.innovecstesttask.data.result_handling.convertStringTypeIntoStateFormat
 import com.example.innovecstesttask.data.repository.ActionConfigRepository
-import com.example.innovecstesttask.data.result_handling.ErrorDataResult
-import com.example.innovecstesttask.data.result_handling.SuccessDataResult
+import com.example.innovecstesttask.data.result_handling.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class MainViewModel : BaseViewModel() {
+class MainViewModel constructor(private val dataManager: ConfigurationDataManager) : BaseViewModel() {
 
     private var mutableLiveDataState: MutableLiveData<BaseState> = MutableLiveData()
     val viewState: LiveData<BaseState> = mutableLiveDataState
+
+//    override fun getDataManager() = ConfigurationDataManager()
 
     override fun obtainIntent(intent: BaseUserIntent) {
         mutableLiveDataState.value = BaseState.LoadingState
@@ -31,7 +30,7 @@ class MainViewModel : BaseViewModel() {
                     when (resultData) {
                         is SuccessDataResult -> {
                             val state =
-                                ConfigurationDataManager().getHandledData(resultData.data).type.convertStringTypeIntoStateFormat()
+                                dataManager.getHandledData(resultData.data).type.convertStringTypeIntoStateFormat()
                             mutableLiveDataState.value = state
                         }
                         is ErrorDataResult -> {
