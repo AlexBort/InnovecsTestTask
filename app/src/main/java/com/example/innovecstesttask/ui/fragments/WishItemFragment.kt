7e3f5170943a1db8bf0dcac06c211eb6890b90dd
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2023-07-05 17:32:06 +0300

    rewrite logic: worked first request

--- app/src/main/java/com/example/innovecstesttask/ui/fragments/WishItemFragment.kt
+++ app/src/main/java/com/example/innovecstesttask/ui/fragments/WishItemFragment.kt
@@ -2,11 +2,15 @@ package com.example.innovecstesttask.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.example.innovecstesttask.R
 import com.example.innovecstesttask.viewModel.WishItemViewModel
 
-class WishItemFragment : Fragment() {
+class WishItemFragment : Fragment(R.layout.fragment_wish_item) {
 
     private val viewModel: WishItemViewModel by viewModels()
 
@@ -15,9 +19,16 @@ class WishItemFragment : Fragment() {
 
         val itemId = arguments?.getString("itemId") ?: return
 
+        val imageView = view.findViewById<ImageView>(R.id.image)
+        val titleView = view.findViewById<TextView>(R.id.title)
+        val idView = view.findViewById<TextView>(R.id.id)
+
         viewModel.fetchWishlistItem(itemId)
-        viewModel.wishItem.observe(viewLifecycleOwner, { item ->
+        viewModel.wishItem.observe(viewLifecycleOwner) { item ->
             // update UI
-        })
+            Glide.with(imageView.context).load(item.hardcodedImageLink).into(imageView)
+            titleView.text = item.title
+            idView.text = item.id
+        }
     }
 }
